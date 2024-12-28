836d9bff839a79db72cac810aab4d3a31b036514
Victor Redondo <vrviktor@gmail.com>
2024-12-28 01:14:20 +0100

    Add checking jokes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
+const reportJokes: Array<{ joke: string; score: number; date: string }> = [];
 const showJoke = async (): Promise<void> => {
   const newJoke = document.getElementById("newJoke");
+  const selectedEmoji = document.querySelector(
+    "input[name='emojiRating']:checked"
+  ) as HTMLInputElement | null;
+
+  const emojiRating = selectedEmoji ? parseInt(selectedEmoji.value) : null;
+
   const url = "https://icanhazdadjoke.com/";
   const options = {
     method: "GET",
@@ -11,7 +18,25 @@ const showJoke = async (): Promise<void> => {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    newJoke ? (newJoke.textContent = result.joke) : console.log("error");
+    if (newJoke) {
+      newJoke.textContent = result.joke;
+      const currentDate = new Date().toISOString();
+      if (emojiRating) {
+        let joke = {
+          joke: result.joke,
+          score: emojiRating,
+          date: currentDate,
+        };
+        reportJokes.push(joke);
+        console.log(reportJokes);
+
+        if (selectedEmoji) {
+          selectedEmoji.checked = false;
+        }
+      }
+    } else {
+      console.log("error");
+    }
   } catch (error) {
     console.log("Joke not found", error);
   }
